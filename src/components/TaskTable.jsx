d2402ede748a7a389163608483637bdfe9163962
Alexis-Torrealba <alexistorrealba1996@gmail.com>
2023-11-16 01:23:46 -0400

    To-do V-1

---
+++ src/components/TaskTable.jsx
@@ -0,0 +1,24 @@
+import { TaskRow } from './TaskRow';
+
+const TaskTable = ({ tasks, toggleTask, showCompleted = false }) => {
+	const taskTableRows = (doneValue) => {
+		return tasks
+			.filter((task) => task.done === doneValue)
+			.map((task) => (
+				<TaskRow task={task} key={task.name} toggleTask={toggleTask} />
+			));
+	};
+
+	return (
+		<table className="table table-dark table-striped table-bordered border-segundary">
+			<thead>
+				<tr className="table-primary">
+					<th>Tasks</th>
+				</tr>
+			</thead>
+			<tbody>{taskTableRows(showCompleted)}</tbody>
+		</table>
+	);
+};
+
+export { TaskTable };
